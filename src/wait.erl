b843ff511107acd3e6dba86c0548bd0a741c2583
Gregory Burd <greg@basho.com>
2012-11-26 14:00:35 -0500

    Looks stable.

--- src/wait.erl
+++ src/wait.erl
@@ -2,20 +2,7 @@
 -compile(export_all).
 -on_load(init/0).
 
--define(ASYNC_NIF_CALL(Fun, Args),
-        begin
-            Ref = erlang:make_ref(),
-            case erlang:apply(Fun, [Ref|Args]) of
-                {ok, Metric} ->
-                    io:format("queue depth: ~p~n", [Metric]),
-                    erlang:bump_reductions(Metric * 100), %% TODO: 100 is a *guess*
-                    receive
-                        {Ref, Reply} ->
-                            Reply
-                    end;
-                Other -> Other
-            end
-        end).
+-include("async_nif.hrl").
 
 %%  N = number of spawned procs
 %%  W = microseconds spent waiting in NIF
@@ -48,31 +35,14 @@ spawn_n(N, F) ->
 busywait(0) ->
     ok;
 busywait(N) ->
-    %% case ASYNC_NIF_CALL(fun busywait_nif/2, [N]) of
-    %%     {error, shutdown}=Error ->
-    %%         %% Work unit was not executed, requeue it.
-    %%         Error;
-    %%     {error, _Reason}=Error ->
-    %%         %% Work unit returned an error.
-    %%         Error;
-    %%     {ok, Result} ->
-    %%         Result
-    %% end.
+    %% ASYNC_NIF_CALL(fun busywait_nif/2, [N]).
     busywait(N-1).
 
 busywait_nif(_Ref, _Count) ->
     not_loaded.
 
 sleep(Microseconds) ->
-    case ?ASYNC_NIF_CALL(fun sleep_nif/2, [Microseconds]) of
-        {eror, shutdown}=Error ->
-            Error;
-        {error, _Reason}=Error ->
-            Error;
-        {ok, Slept} ->
-            io:format("slept ~p~n", [Slept]),
-            ok
-    end.
+    ?ASYNC_NIF_CALL(fun sleep_nif/2, [Microseconds]).
 
 sleep_nif(_Ref, _Microseconds) ->
     not_loaded.
